55c976f46cb788295f0a2f0e88d8d0d80988108f
Chris Mather <mather.chris@gmail.com>
2014-03-30 11:16:56 -0700

    Add mixpanel tracking.

--- client/app.js
+++ client/app.js
@@ -2,6 +2,29 @@
 /* Client App Namespace  */
 /*****************************************************************************/
 _.extend(App, {
+  track: function (key, meta) {
+    meta = meta || {};
+
+    Deps.autorun(function (c) {
+      if (!Meteor.loggingIn()) {
+        var user = Deps.nonreactive(function () { return Meteor.user(); });
+        var email;
+        
+        if (user && user.emails.length > 0)
+          email = user.emails[0].address;
+        else
+          email = 'anonymous';
+
+        _.extend(meta, {
+          email: email,
+          path: location.pathname
+        });
+
+        mixpanel.track(key, meta);
+        c.stop();
+      }
+    });
+  }
 });
 
 App.helpers = {
@@ -9,4 +32,9 @@ App.helpers = {
 
 _.each(App.helpers, function (helper, key) {
   Handlebars.registerHelper(key, helper);
+});
+
+Deps.autorun(function () {
+  var path = IronLocation.path();
+  App.track('Page Views');
 });
--- client/controllers/todos/todos_detail.js
+++ client/controllers/todos/todos_detail.js
@@ -1,4 +1,8 @@
 TodosDetailController = RouteController.extend({
+  onRun: function () {
+    App.track('Todos Detail View');
+  },
+
   waitOn: function () {
     var id = this.params._id;
     return [Meteor.subscribe('todos_detail', this.params._id)];
@@ -6,7 +10,12 @@ TodosDetailController = RouteController.extend({
 
   data: function () {
     var id = this.params._id;
-    return Todos.findOne({_id: id});
+    var todo = Todos.findOne({_id: id});
+
+    if (todo)
+      App.track('Todo View', {subject: todo.subject});
+
+    return todo;
   },
 
   action: function () {
